5946519071231309e1e351afae83c728513a1180
bhavukarora03 <bhavuk.arora03@gmail.com>
2023-09-28 15:29:55 +0530

    add: edit button

--- src/components/StayCard/StayCardForOwerProperty.tsx
+++ src/components/StayCard/StayCardForOwerProperty.tsx
@@ -7,20 +7,31 @@ import { Link } from "react-router-dom";
 import BtnLikeIcon from "components/BtnLikeIcon/BtnLikeIcon";
 import SaleOffBadge from "components/SaleOffBadge/SaleOffBadge";
 import Badge from "shared/Badge/Badge";
+import BtnEditIcon from "components/BtnEditIcon/editButton";
+import { on } from "events";
 
 export interface StayCardForOwerPropertyProps {
   className?: string;
   ratioClass?: string;
   data?: StayDataType;
   size?: "default" | "small";
+  onEditNavigate?: () => void;
 }
 
-//const DEMO_DATA = DEMO_STAY_LISTINGS[0];
+const handleCardClick = () => {
+
+  if (onEditNavigate) {
+    onEditNavigate();
+  } else {
+    window.location.href = data.href; 
+  }
+};
 
 const StayCardForOwerProperty: FC<StayCardForOwerPropertyProps> = ({
   size = "default",
   className = "",
   data = "",
+  onEditNavigate,
   ratioClass,
 }) => {
   const {
@@ -31,6 +42,7 @@ const StayCardForOwerProperty: FC<StayCardForOwerPropertyProps> = ({
     bedrooms,
     href,
     like,
+    edit,
     saleOff,
     isAds,
     price,
@@ -50,7 +62,13 @@ const StayCardForOwerProperty: FC<StayCardForOwerPropertyProps> = ({
           ratioClass={ratioClass}
           galleryImgs={galleryImgs}
         />
+
         <BtnLikeIcon isLiked={like} className="absolute right-3 top-3 z-[1]" />
+        <BtnEditIcon
+          onClick={onEditNavigate}
+          className="absolute right-14 top-3 z-[1]"
+        />
+
         {saleOff && <SaleOffBadge className="absolute left-3 top-3" />}
       </div>
     );
@@ -61,7 +79,10 @@ const StayCardForOwerProperty: FC<StayCardForOwerPropertyProps> = ({
       <div className={size === "default" ? "p-4 space-y-4" : "p-3 space-y-2"}>
         <div className="space-y-2">
           <span className="text-sm text-neutral-500 dark:text-neutral-400">
-          <span><i class="las la-bed text-lg"></i> {bedrooms}</span>  <i class="las la-bath text-lg"></i> {bathrooms} 
+            <span>
+              <i class="las la-bed text-lg"></i> {bedrooms}
+            </span>{" "}
+            <i class="las la-bath text-lg"></i> {bathrooms}
           </span>
           <div className="flex items-center space-x-2">
             {isAds && <Badge name="ADS" color="green" />}
@@ -110,10 +131,8 @@ const StayCardForOwerProperty: FC<StayCardForOwerPropertyProps> = ({
               </span>
             )}
           </span>
-          <Link to={'PropertyPortions/'+pid}>View Portions</Link>
-          {!!reviewStart && (
-            <StartRating reviewCount={reviewCount}  />
-          )}
+          <Link to={"PropertyPortions/" + pid}>View Portions</Link>
+          {!!reviewStart && <StartRating reviewCount={reviewCount} />}
         </div>
       </div>
     );
@@ -123,11 +142,10 @@ const StayCardForOwerProperty: FC<StayCardForOwerPropertyProps> = ({
     <div
       className={`nc-StayCard group relative bg-white dark:bg-neutral-900 border border-neutral-100 dark:border-neutral-800 rounded-2xl overflow-hidden hover:shadow-xl transition-shadow ${className}`}
       data-nc-id="StayCard"
+      onClick={handleCardClick} // Handle click on the entire card
     >
-      <Link to={href}>
-        {renderSliderGallery()}
-        {renderContent()}
-      </Link>
+      {renderSliderGallery()}
+      {renderContent()}
     </div>
   );
 };
